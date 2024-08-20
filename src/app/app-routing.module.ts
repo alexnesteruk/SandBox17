a280c530f0d07cf15c13b19f62c1f07ffff5d7f6
Alex <alexnesteruk@gmail.com>
2024-08-19 22:30:27 -0400

    update configurations

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
 import {RouterModule, Routes} from "@angular/router";
+import {HomeComponent} from "./home/home.component";
+import {AboutComponent} from "./about/about.component";
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
@@ -12,4 +14,4 @@ const routes: Routes = [
     RouterModule
   ]
 })
-export class AppRoutingModuleTsModule { }
+export class AppRoutingModule { }
